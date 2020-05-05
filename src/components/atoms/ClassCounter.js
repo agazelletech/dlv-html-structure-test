9c2d757d957353c1981ac0ba939e0c25f283207e
André Wells <agazellecloud@me.com>
2020-05-04 19:10:38 -0500

    Initial commit—added h1-hidden tag in header

---
+++ src/components/atoms/ClassCounter.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class ClassCounter extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       count: 0
+    }
+  }
+  
+
+  render() {
+    return (
+      <>
+          
+        
+      </ >
+    )
+  }
+}
+
+export default ClassCounter
---
+++ src/components/atoms/Counter.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-component';
+const Counter = styled. section`
+  
+`;
+export default Counter;
+
+this.setState = this.setState.bind(this);
